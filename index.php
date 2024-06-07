079ce87f41dc1a5357c1cbc0e4c12d4ea375636a
dannyken21 <iteritekadanny54@gmail.com>
2024-06-07 07:05:16 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav>
+        <a href="#home">HOME</a>
+        <a href="#band">BAND</a>
+        <a href="#tour"> TOUR</a>
+        <a href="#contact">CONTACT</a>
+        <div class="icon">
+            <img src="icon.png" alt="search">
+        </div>
+    </nav>
+
+    <section class="home" id="home">
+        <img src="image.jpg" alt="">
+        <center>
+            <p style="font-size: 25px;">
+                <b>KIGALI CITY</b>
+            </p>
+            <p style="font-size: 15px;">We had the best time playing at Kigali Convetion Centre</p>
+        </center>
+    </section><br>
+    <div class="center">
+        <section id="band">
+            <center><br><br><br>
+                <h1>THE BAND</h1><br>
+                <p><i>We love music</i><br><br></p>
+            </center>
+
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, itaque voluptatibus accusantium ratione
+            esse
+            asperiores dolorem corporis molestiae illum. Labore fugit consequuntur vero explicabo illum obcaecati quis
+            doloribus, ducimus modi.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, itaque
+            voluptatibus accusantium ratione esse
+            asperiores dolorem corporis molestiae illum. Labore fugit consequuntur vero explicabo illum obcaecati quis
+            doloribus, ducimus modi. <br>
+            <center>
+                <div class="box">
+                    name <br><br>
+                    <img src="guital.jpg" alt="">
+                </div>
+                <div class="box">
+                    name <br><br>
+                    <img src="guital.jpg" alt="">
+                </div>
+                <div class="box">
+                    name <br><br>
+                    <img src="guital.jpg" alt="">
+                </div>
+    </div>
+    </center>
+    </section>
+    <div class="back">
+        <div class="center">
+            <section id="tour">
+                <div class="dates">
+                    <center><br><br>
+                        <h1>TOUR DATES</h1><br>
+                        <p><i>Remember to book your tickets!</i></p>
+                    </center><br>
+                    <section class="sales">
+                        <p>September: <span class="sout">sold out</span></p>
+
+                        <p>October: <span class="sout">sold out</span></p>
+
+                        <p>November: <span class="number">3</span></p>
+                    </section>
+                    <section class="boxes">
+                </div>
+                <div class="all">
+                    <div class="img">
+                        <img src="new york.jpg" alt="">
+                        <p><b>New York</b></p>
+                        <p>Fri 27Nov2016</p>
+                        <p>Lorem, ipsum dolor sit amet <br>consectetur adipisicing elit.<br> Adipisci cum beatae
+                            voluptatum,
+                            vero
+                        </p>
+                        <button>Buy Tickets</button>
+                    </div>
+                    <div class="img">
+                        <img src="paris.jpg" alt="">
+                        <p><b>Paris</b></p>
+                        <p>Sat 27Nov2016</p>
+                        <p>Lorem, ipsum dolor sit amet <br>consectetur adipisicing elit.<br> Adipisci cum beatae
+                            voluptatum,
+                            vero.</p>
+                        <button>Buy Tickets</button>
+                    </div>
+                    <div class="img">
+                        <img src="san fransisco.jpg" alt="">
+                        <p><b>San Francisco</b></p>
+                        <p>Sun 27Nov2016</p>
+                        <p>Lorem, ipsum dolor sit amet <br>consectetur adipisicing elit.<br> Adipisci cum beatae
+                            voluptatum,
+                            vero</p>
+                        <button>Buy Tickets</button>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- </section> -->
+    </div>
+    <section id="contact">
+        <center>
+            <h1><br><br><br>
+                CONTACT
+            </h1>
+            <p style="font-size: 10px;"><i>Fan? Drop a note!</i></p>
+        </center>
+        <center>
+            <div class="center">
+                <div class="in">
+                    <span class="left">
+                        <img src="location.png" alt="location" class="icon">Chicago,US <br>
+                        <img src="phone.png" alt="phone" class="icon">Phone:+00 151515 <br>
+
+                        <img src=" mail.png" alt="email" class="icon">Email:mail@mail.com <br>
+                    </span>
+                    <div class="right">
+                        <form action="">
+                            <input type="text" placeholder="Name"><input type="email" placeholder="Email"
+                                style="margin-left: 10px;"><br>
+                            <textarea name="" id="" cols=" 30" rows="10" placeholder="Message"></textarea>
+                            <div class="button"><button>SEND</button></div>
+                        </form>
+                    </div>
+        </center>
+        </div>
+        </div>
+    </section>
+    </div>
+</body>
+
+</html>
